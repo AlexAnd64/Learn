ab0d6813675d8ff0142f6c44d2389fa60fef8193
Alexander <lapostov@yandex.ru>
2026-03-24 16:05:40 +0400

    Исправил недочеты и удалил папки с работами не относящимися к данному дз (lesson_5)

--- src/main/java/lesson_2/Task1.java
+++
@@ -1,7 +0,0 @@
-package lesson_2;
-
-public class Task1 {
-    public String printThreeWords() {
-        return ("Orange\nBanana\nApple");
-    }
-}
--- src/main/java/lesson_5/ArrayCheckMethod.java
+++ src/main/java/lesson_5/ArrayCheckMethod.java
@@ -1,36 +1,16 @@
 package lesson_5;
 
-class MyArraySizeException extends Exception {
-    public MyArraySizeException(String message) {
-        super(message);
-    }
-}
-
-class MyArrayDataException extends Exception {
-    public MyArrayDataException(String message) {
-        super(message);
-    }
-}
-
 public class ArrayCheckMethod {
-    public ArrayCheckMethod() {
-    }
-
     public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
-        // Проверка размера массива
         if (array.length != 4) {
             throw new MyArraySizeException("Массив должен иметь 4 строки, получено: " + array.length);
         }
-
         for (int i = 0; i < array.length; i++) {
             if (array[i].length != 4) {
                 throw new MyArraySizeException("Строка " + i + " должна содержать 4 элемента, получено: " + array[i].length);
             }
         }
-
         int sum = 0;
-
-        // Проход по всем элементам и преобразование в int
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 try {
@@ -40,7 +20,6 @@ public class ArrayCheckMethod {
                 }
             }
         }
-
         return sum;
     }
 }
